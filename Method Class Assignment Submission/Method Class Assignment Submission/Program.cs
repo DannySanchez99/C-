6ce3f1504ca1aa808e31ed8affd5b34baf64969b
DannySanchez99 <132256950+DannySanchez99@users.noreply.github.com>
2026-03-05 23:08:33 -0500

    Add files via upload

---
+++ Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+
+class MathOperations
+{
+    // Create a void method that takes two integers as parameters
+    // The method performs a math operation on the first integer
+    public void DoMath(int number1, int number2)
+    {
+       
+        int result = number1 * 2; // Perform a math operation on the first integer (example: multiply by 2)
+        Console.WriteLine("Result of math operation on first number: " + result);   // Display the result of the math operation     
+        Console.WriteLine("Second number is: " + number2); // Display the second integer
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        MathOperations mathObject = new MathOperations();  // Instantiate create the MathOperations class
+
+        // Call the method and pass two integers normally
+        mathObject.DoMath(5, 10);
+        mathObject.DoMath(number1: 7, number2: 20);
+      
+        Console.ReadLine(); // Wait for user 
+    }
+}
---
+++ Method Class Assignment Submission/Method Class Assignment Submission/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Method Class Assignment Submission")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Method Class Assignment Submission")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd45f8d7-1bff-4daf-bf80-2eb26ff7aa70")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
