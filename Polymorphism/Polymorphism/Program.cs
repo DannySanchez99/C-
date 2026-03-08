edfeb27d43bbadb04d622465618dfbcf5b761875
DannySanchez99 <132256950+DannySanchez99@users.noreply.github.com>
2026-03-07 22:19:12 -0500

    Add files via upload

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System; 
+
+public interface IQuittable
+{
+    
+    void Quit();
+}
+
+class Employee : IQuittable
+{
+    public string Name { get; set; } // Property to store the employee's name
+
+    public int Id { get; set; } // Property to store the employee's ID
+
+    public void Quit() // Implementation of the Quit() method required by the interface
+    {
+        Console.WriteLine(Name + " has quit the job."); // This message will appear in the console when the method is called
+    }
+}
+
+class Program
+{
+    static void Main(string[] args) // The entry point of the console application
+    {
+        Employee emp = new Employee(); // Create a new Employee        
+        emp.Name = "Danny";
+        emp.Id = 1;
+
+        IQuittable quittableEmployee = emp; // POLYMORPHISM: Create a variable of type IQuittable
+
+        quittableEmployee.Quit(); // Call the Quit() method through the interface reference
+
+
+        Console.ReadLine(); // Keeps the console window open until the user presses Enter
+    }
+}
